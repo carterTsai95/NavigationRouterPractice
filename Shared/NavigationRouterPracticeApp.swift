654c53e2e166f81602bc80cc606daf6840f67431
Hung-Chun(Carter) Tsai <carterTsai95@gmail.com>
2022-07-15 21:53:17 -0400

    Initial Commit

---
+++ Shared/NavigationRouterPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationRouterPracticeApp.swift
+//  Shared
+//
+//  Created by Hung-Chun Tsai on 2022-07-15.
+//
+
+import SwiftUI
+
+@main
+struct NavigationRouterPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
